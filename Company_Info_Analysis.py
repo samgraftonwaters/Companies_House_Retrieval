5b273fa644e8e9879a7830ba3492894df1220641
Sam Grafton-Waters <sjww11@gmail.com>
2026-08-14 12:52:52 +0100

    added overview function to main script

--- Company_Info_Analysis.py
+++ Company_Info_Analysis.py
@@ -4,9 +4,20 @@ import numpy as np
 import time
 
 import Call_API_Functions as CAF
+import Overview
 
 
 api_key = None
 url = "https://api.company-information.service.gov.uk/company/"
 
 companies = []
+
+def get_company_info():
+
+    Overview.overview_data(companies = companies, url = url, api_key = api_key)
+
+
+
+if __name__ == '__main__':
+
+    get_company_info()
