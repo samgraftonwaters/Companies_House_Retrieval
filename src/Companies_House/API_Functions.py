32660bef3bfad254b8ca72ea507d704d8007d673
Sam Grafton-Waters <sjww11@gmail.com>
2026-09-02 09:01:04 +0100

    updated API function to include params

--- src/Companies_House/API_Functions.py
+++ src/Companies_House/API_Functions.py
@@ -3,14 +3,13 @@ import pandas as pd
 import numpy as np
 import time
 
-def call_api(url, api_key, company_number, company_info):
+def call_api(url, api_key, company_number, company_info, params = None):
     
-    response = requests.get(url = url + company_number + company_info, auth = (api_key, ""))
-    time.sleep(0.1)
+    response = requests.get(url = url + company_number + company_info, params=params, auth = (api_key, ""))
+    time.sleep(0.05)
     
     if response.status_code not in (200, 201):
         return(None)
-
     else:
         return(response.json())
 
